3d1a3eb9d7f6ff6af3cc92e65f2ec58be1ed1b6d
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-17 21:22:46 +0300

    add flake8 settings, add related_name to book&user fields in model Borrowing, add file to .gitignore

--- borrowings/models.py
+++ borrowings/models.py
@@ -8,8 +8,8 @@ class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="borrowings")
 
     def __str__(self):
         return f"Borrowing of {self.book.title} by {self.user.email}"
