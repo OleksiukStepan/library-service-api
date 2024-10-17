05b7e6eb9b451db55755f071ade9b555084ac6be
Stepan <oleksiukstepann@gmail.com>
2024-10-17 14:54:18 +0300

    rename user to users

--- library/models.py
+++ library/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from decimal import Decimal
 
-from user.models import User
+from users.models import User
 
 
 class Book(models.Model):
