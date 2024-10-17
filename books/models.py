95583be134b15e038f310405b1a33fded321ffee
Misha Havryliuk <misharob31415@gmail.com>
2024-10-17 16:18:45 +0300

    create model Book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,24 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=CoverType.choices)
+    inventory = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    daily_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.01"))]
+    )
+    image = models.ImageField(upload_to="book_images/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
