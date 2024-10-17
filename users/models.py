4e87eaed71338b7981ab27528966a9105beda486
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-17 18:52:56 +0300

    modified users/models.py

--- users/models.py
+++ users/models.py
@@ -8,8 +8,8 @@ from users.manager import UserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    first_name = models.CharField(_("first name"), max_length=30)
-    last_name = models.CharField(_("last name"), max_length=30)
+    first_name = models.CharField(_("first name"), max_length=63)
+    last_name = models.CharField(_("last name"), max_length=63)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
