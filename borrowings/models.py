e23b478b29de63fdf184f61776805672d4572fe4
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-17 20:38:10 +0300

    create an app borrowings, add model Borrowing

---
+++ borrowings/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from users.models import User
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Borrowing of {self.book.title} by {self.user.email}"
