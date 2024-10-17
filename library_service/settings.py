708002a9ddfabd2a1515c868033e0bc7fb3510ad
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-17 20:41:53 +0300

    set new requirements.txt, configured the settings.py, add urls.py for borrowing app

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # 3rd apps
+    "rest_framework",
 
     # my apps
     "library",
     "users",
+    "borrowings",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
