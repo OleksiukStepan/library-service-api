8c37a1bbc8c4d60322b751d9183449636bcbe0ac
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-18 14:21:42 +0300

    add remove chat id from db function if admin blocks telegram bot

--- notifications/utils.py
+++ notifications/utils.py
@@ -3,6 +3,15 @@ from django.contrib.auth import get_user_model
 
 
 @sync_to_async
+def remove_chat_id(chat_id: int) -> None:
+    user = get_user_model().objects.filter(telegram_chat_id=chat_id).first()
+
+    if user:
+        user.telegram_chat_id = None
+        user.save()
+
+
+@sync_to_async
 def get_admin_chat_ids() -> set:
 
     return set(
