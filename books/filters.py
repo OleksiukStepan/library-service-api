f797181300d26dc28837899a31fb7810dcedf700
Misha Havryliuk <misharob31415@gmail.com>
2024-10-18 16:01:23 +0300

    add book filters for title and author

---
+++ books/filters.py
@@ -0,0 +1,11 @@
+from django_filters import rest_framework as filters
+from books.models import Book
+
+
+class BookFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name="title", lookup_expr="icontains")
+    author = filters.CharFilter(field_name="author", lookup_expr="icontains")
+
+    class Meta:
+        model = Book
+        fields = ["title", "author"]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from books.filters import BookFilter
 from books.models import Book
 from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
@@ -9,3 +10,4 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filterset_class = BookFilter
