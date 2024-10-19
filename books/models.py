230b820509d63a87bd3c7d2b301748233f217b22
Misha Havryliuk <misharob31415@gmail.com>
2024-10-19 13:39:02 +0300

    change ordering

--- books/models.py
+++ books/models.py
@@ -30,8 +30,5 @@ class Book(models.Model):
     )
     image = models.ImageField(upload_to=books_image_file_path, null=True, blank=True)
 
-    class Meta:
-        ordering = ["title", "author"]
-
     def __str__(self):
         return f"{self.title} by {self.author}"
