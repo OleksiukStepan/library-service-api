c7680ad07fe7979591d1e0a58244baa4587c6a6d
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-19 14:53:50 +0300

    add 'Payment' views
    regular ViewSet with limited access for users
    SuccessView and CancelView to operate
    strip redirections

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+import stripe
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentUserSerializer, PaymentStaffSerializer
+
+
+class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_staff:
+            return Payment.objects.filter(borrowing__user=user)
+        return Payment.objects.all()
+
+    def get_serializer_class(self):
+        user = self.request.user
+        if user.is_staff:
+            return PaymentStaffSerializer
+        return PaymentUserSerializer
+
+class PaymentSuccessView(APIView):
+    def get(self, request):
+        session_id = request.query_params.get("session_id")
+        payment = get_object_or_404(Payment, session_id=session_id)
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        if session.payment_status == "paid":
+            payment.status = Payment.Status.PAID
+            payment.save()
+
+            return Response({"message": "Payment successful"}, status=200)
+        else:
+            return Response({"message": "Payment not completed"}, status=400)
+
+
+class PaymentCancelView(APIView):
+    def get(self, request):
+        return Response(
+            {"message": "Payment was cancelled. You can try again within 24 hours."},
+            status=200,
+        )
