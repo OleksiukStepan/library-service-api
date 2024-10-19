c5d5de4bbc33aeab6f94ad613a3cb04edb0db03f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-20 00:37:15 +0300

    fix check expired sessions function by adding try-except block

--- payments/tasks.py
+++ payments/tasks.py
@@ -13,7 +13,12 @@ def check_expired_sessions() -> None:
     pending_payments = Payment.objects.filter(status=Payment.Status.PENDING)
 
     for payment in pending_payments:
-        session = stripe.checkout.Session.retrieve(payment.session_id)
-        if session.payment_status == "expired":
+        try:
+            session = stripe.checkout.Session.retrieve(payment.session_id)
+            if session.status != "open":
+                payment.status = Payment.Status.EXPIRED
+                payment.save()
+
+        except stripe.error.InvalidRequestError:
             payment.status = Payment.Status.EXPIRED
             payment.save()
