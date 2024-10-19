8c783dd17c69c7f1440e7fdd2069ca9c7b4affbc
Misha Havryliuk <misharob31415@gmail.com>
2024-10-19 12:28:40 +0300

    add schema view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 
 from books.filters import BookFilter
@@ -12,3 +14,20 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     permission_classes = (IsAdminOrReadOnly,)
     filterset_class = BookFilter
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "title",
+                type=OpenApiTypes.STR,
+                description="Filter by book title (ex. ?title=Harry Potter)",
+            ),
+            OpenApiParameter(
+                "author",
+                type=OpenApiTypes.STR,
+                description="Filter by author name (ex. ?author=Tolkien)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
