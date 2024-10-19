613488619ef0659f4a5ccfd44c66e4572354d316
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-19 15:01:30 +0300

    add django secret key to .env

--- library_service/settings.py
+++ library_service/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)+zax=5j7gp=n_evcf52i!p42%g-+-q%^hjn+^%n=optkdk54@"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
