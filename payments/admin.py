0b97523560acffeeb1c7d753ae78e6d048368ae3
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-19 14:48:36 +0300

    refactor 'Payment' in admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from payments.models import Payment
 
-admin.site.register(Payment)
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "status",
+        "type",
+        "borrowing",
+        "session_id",
+        "money_to_pay",
+    )
+    list_filter = (
+        "status",
+        "type",
+        "borrowing",
+    )
