ca33f4e014a11249243dcac746d76349226e5557
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-20 17:39:13 +0300

    fix: tests for filters, removed additional mocks

--- borrowings/tests/test_filters.py
+++ borrowings/tests/test_filters.py
@@ -50,43 +50,45 @@ class BorrowingFilterTest(TestCase):
             daily_fee=1.50,
         )
 
-        with patch("borrowings.views.send_telegram_message"):
-            self.borrowing1 = Borrowing.objects.create(
-                borrow_date="2023-01-01",
-                expected_return_date="2023-01-10",
-                book=self.book1,
-                user=self.user1,
-            )
-            self.borrowing2 = Borrowing.objects.create(
-                borrow_date="2023-01-05",
-                expected_return_date="2023-01-15",
-                book=self.book2,
-                user=self.user2,
-            )
-            self.borrowing3 = Borrowing.objects.create(
-                borrow_date="2023-01-10",
-                expected_return_date="2023-01-20",
-                actual_return_date="2023-01-18",
-                book=self.book1,
-                user=self.user1,
-            )
+        self.borrowing1 = Borrowing.objects.create(
+            borrow_date="2023-01-01",
+            expected_return_date="2023-01-10",
+            book=self.book1,
+            user=self.user1,
+        )
+        self.borrowing2 = Borrowing.objects.create(
+            borrow_date="2023-01-05",
+            expected_return_date="2023-01-15",
+            book=self.book2,
+            user=self.user2,
+        )
+        self.borrowing3 = Borrowing.objects.create(
+            borrow_date="2023-01-10",
+            expected_return_date="2023-01-20",
+            actual_return_date="2023-01-18",
+            book=self.book1,
+            user=self.user1,
+        )
 
         self.url = reverse("borrowings:borrowings-list")
         self.client.login(email="test_admin@example.com", password="1qazcde3")
 
     def test_filter_by_is_active(self):
+        self.client.force_authenticate(user=self.admin)
         response = self.client.get(self.url, {"is_active": "true"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         borrowings = Borrowing.objects.filter(actual_return_date__isnull=True)
         self.assertEqual(len(response.data), borrowings.count())
 
     def test_filter_by_user_id(self):
+        self.client.force_authenticate(user=self.admin)
         response = self.client.get(self.url, {"user_id": self.user1.id})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         borrowings = Borrowing.objects.filter(user=self.user1)
         self.assertEqual(len(response.data), borrowings.count())
 
     def test_filter_by_is_active_and_user_id(self):
+        self.client.force_authenticate(user=self.admin)
         response = self.client.get(
             self.url, {"is_active": "true", "user_id": self.user1.id}
         )
